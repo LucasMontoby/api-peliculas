80ea06d95e3f9975caf3872f1251e0e67ce0f18d
LUCAS\monto <lucasmontoby@gmail.com>
2024-05-10 19:11:57 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useEffect, useState } from 'react';
+import './style.css';
 
 function App() {
+
+  const [peliculas, setPeliculas] = useState([])
+
+  useEffect (() => {
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=1c3fab805396314881b22647f450a7da')
+    .then(response => response.json())
+    .then(data => setPeliculas(data.results))
+    .catch(e => console.error('Errro al obtener pelíclas: ', e))
+    },[])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1 className="titulo">Listado de Películas Populares </h1>
+    <ul>
+      {peliculas.map(pelicula =>(
+        <div>
+          <li key={pelicula.id}>
+            <img src={`https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg${pelicula.poster_path}`} alt={peliculas.title}
+            />
+          <h2>{pelicula.title}</h2>
+          <p>Puntuación: {pelicula.vote_average}</p>
+          </li>
+        </div>
+     
+      ))}
+    </ul>
+  </div>
   );
 }
 
